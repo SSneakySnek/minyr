f9bdee8a265dc2ef6b905c4473f2ecba946ce4e7
SSneakySnek <thomasn303@gmail.com>
2023-03-22 01:44:37 +0100

    commit

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	var input string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		input = scanner.Text()
+		if input == "q" || input == "exit" {
+			fmt.Println("Exit")
+			os.Exit(0)
+		} else if input == "convert" {
+			fmt.Println("Konverterer alle målingene gitt i grader Celsius til grader Farenheit.")
+
+		} else {
+			fmt.Println("Venligst velg convert, average eller exit:")
+		}
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- yr/yr_test.go
+++ yr/yr_test.go
@@ -1,1 +1,16 @@
+package yr
 
+import (
+	"fmt"
+	"path/to/yr"
+)
+
+func test() {
+	filepath := "C:/Users/thomas/Desktop/visualCoding/minyr/kjevik-temp-celsius-20220318-20230318.csv"
+	numLines, err := yr.CountLinesInCSVFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Number of lines in the CSV file: %d", numLines)
+}
