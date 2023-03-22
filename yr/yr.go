ecec20c99aff8a39866c9cf9ac948a454a020370
SSneakySnek <thomasn303@gmail.com>
2023-03-22 01:44:28 +0100

    commit

--- yr/yr.go
+++ yr/yr.go
@@ -1,1 +1,23 @@
+package yr
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func testingForLines() {
+	file, err := os.Open("thomas - Personal/Desktop/visualCoding/minyr/kjevik-temp-celsius-20220318-20230318.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lines := 0
+	for scanner.Scan() {
+		lines++
+	}
+
+	fmt.Printf("Filen inneholder så mange linjer: %d", lines)
+}
